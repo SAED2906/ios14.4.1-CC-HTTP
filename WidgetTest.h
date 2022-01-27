e26911ab1b3c35b6132469fe094b5e541c374276
William Marais <79162124+SAED2906@users.noreply.github.com>
2022-01-27 17:42:21 +0200

    Add files via upload

--- WidgetTest.h
+++ WidgetTest.h
@@ -1,5 +1,10 @@
 #import <ControlCenterUIKit/CCUIToggleModule.h>
+#import <UIKit/UIKit.h>
 
 @interface WidgetTest : CCUIToggleModule
+
+@end
+
+@interface ViewController : UIViewController
 
 @end
--- WidgetTest.m
+++ WidgetTest.m
@@ -1,31 +1,18 @@
 #import "WidgetTest.h"
+#include <UIKit/UIKit.h>
 
 @implementation WidgetTest
-
-// Most third-party Control Center modules out there use non-CAML approach because it's easier to get icon images than create CAML
-// Choose either CAML and non-CAML portion of the code for your final implementation of the toggle
-// IMPORTANT: To prepare your icons and configure the toggle to its fullest, check out CCSupport Wiki: https://github.com/opa334/CCSupport/wiki
-
-#pragma mark - CAML approach
-
-// CAML descriptor of your module (.ca directory)
-// Read more about CAML here: https://medium.com/ios-creatix/apple-make-your-caml-format-a-public-api-please-9e10ba126e9d
-- (CCUICAPackageDescription *)glyphPackageDescription {
-    return [CCUICAPackageDescription descriptionForPackageNamed:@"WidgetTest" inBundle:[NSBundle bundleForClass:[self class]]];
-}
-
-#pragma mark - End CAML approach
 
 #pragma mark - Non-CAML approach
 
 // Icon of your module
 - (UIImage *)iconGlyph {
-    return [UIImage imageNamed:@"icon" inBundle:[NSBundle bundleForClass:[self class]] compatibleWithTraitCollection:nil];
+    return [UIImage imageNamed:@"ModuleIcon@2x" inBundle:[NSBundle bundleForClass:[self class]] compatibleWithTraitCollection:nil];
 }
 
 // Optional: Icon of your module, once selected 
 - (UIImage *)selectedIconGlyph {
-    return [UIImage imageNamed:@"icon-selected" inBundle:[NSBundle bundleForClass:[self class]] compatibleWithTraitCollection:nil];
+    return [UIImage imageNamed:@"ModuleIcon@2x" inBundle:[NSBundle bundleForClass:[self class]] compatibleWithTraitCollection:nil];
 }
 
 // Selected color of your module
@@ -40,8 +27,36 @@
     return NO;
 }
 
+
+
+
 - (void)setSelected:(BOOL)selected {
     if (selected) {
+	
+	UIAlertController *alert = [UIAlertController alertControllerWithTitle:@"Title"
+                                                                           message:@"Message"
+                                                                    preferredStyle:(UIAlertControllerStyleAlert)];
+
+	UIAlertAction *cancelAction = [UIAlertAction actionWithTitle:@"Cancel"
+                                                    style:UIAlertActionStyleCancel
+                                                  handler:^(UIAlertAction * _Nonnull action) {
+                                                         // handle cancel response here. Doing nothing will dismiss the view.
+                                                  }];
+// add the cancel action to the alertController
+[alert addAction:cancelAction];
+
+// create an OK action
+UIAlertAction *okAction = [UIAlertAction actionWithTitle:@"OK"
+                                                   style:UIAlertActionStyleDefault
+                                                 handler:^(UIAlertAction * _Nonnull action) {
+                                                         // handle response here.
+                                                 }];
+// add the OK action to the alert controller
+[alert addAction:okAction];
+
+//[self presentViewController:alert animated:YES completion:^{
+//    // optional code for what happens after the alert controller has finished presenting
+//}];
         // Your module turned selected/on, do something
     } else {
         // Your module turned unselected/off, do something
